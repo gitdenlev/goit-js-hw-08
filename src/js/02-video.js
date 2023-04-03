32c92cc0c5eac02e46cb0161ce88bb1e649287ad
gitdenlev <levchenkoden02@gmail.com>
2023-04-03 17:00:24 +0200

    Fix bug

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
 import Player from '@vimeo/player';
 
-import {throttle} from 'lodash.throttle'
+import throttle from 'lodash.throttle'
 
 const iframe = document.querySelector('iframe');
 
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(e => {
     localStorage.setItem('videoplayer-current-time', e.seconds);
